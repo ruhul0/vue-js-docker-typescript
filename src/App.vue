5e66fc1d91955409752955f06036d10fd000d200
ruhul0 <ruhul20@gmail.com>
2023-05-27 01:03:32 +0600

    initia

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,80 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+ <template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click= "changeName('test')">change names </button>
+    <button @click= "changeAge(30)">change age </button>
+    <!-- <p>{{ jobs[0] }}</p> -->
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+// let age: string | number = 27
+
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './components/types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup(){
+
+    // reactive 
+    // const state = reactive({
+    //   name: 'Link' as String,
+    //   age: 25 as string | number
+    // })
+
+    // state.name = "ruhul"
+    // state.age = 27
+
+    // return { ...toRefs(state) }
+
+    const name = ref('Link')
+    const age = ref<number | string>(25)
+
+    return {name,age}
+
+    // const jobs = ref<Job[]>([
+    //   {
+    //     title:"developer",
+    //     location: "dhaka",
+    //     salary: 300000,
+    //     id: '1'
+    //   },
+    //   {
+    //     title:"developer",
+    //     location: "dhaka",
+    //     salary: 10000,
+    //     id: '2'
+    //   },
+    //   {
+    //     title:"developer",
+    //     location: "dhaka",
+    //     salary: 15000,
+    //     id: '3'
+    //   },
+    //   {
+    //     title:"developer",
+    //     location: "dhaka",
+    //     salary: 20000,
+    //     id: '4'
+    //   }
+    // ])
+  },
+  methods: {
+    changeName(name:string) {
+      this.name = name;
+      return name
+    },
+    changeAge(age:string | number){
+      this.age = age
+      return age
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
